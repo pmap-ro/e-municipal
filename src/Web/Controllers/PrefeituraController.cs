01e92649a611a0ee8a0012426cf46df6341eb9f5
Cleyton Ferrari <cleytonferrari@hotmail.com>
2017-04-28 14:43:48 -0400

    Configurado o minefield/css;

--- src/Web/Controllers/PrefeituraController.cs
+++ src/Web/Controllers/PrefeituraController.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace Web.Controllers
 {
--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -20,6 +20,7 @@ namespace Web
                 .Build();
 
             host.Run();
+
         }
     }
 }
